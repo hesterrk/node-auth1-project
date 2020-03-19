4b3aa3a9478d3114fd19d7c05748929b376f5e24
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-19 22:03:12 +0000

    deleted the first, mvp for back-end auth, started react app to display information

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>My Auth App</h3>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
